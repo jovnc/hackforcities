6fbf6539c1e226731d66a20c54d05f7ca52e8a9e
jovnc <edtokens@gmail.com>
2025-01-10 20:46:06 +0800

    added upload file

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,51 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from helpers.preprocessing import load_vectorstore, vectorstore_ingest
-from helpers.prompts import question_prompt
-from langchain.chains import RetrievalQA
-from langchain_openai import ChatOpenAI
+from helpers.preprocessing import vectorstore_ingest
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({'message': 'Hello from Flask backend!'})
 
-@app.route('/api/upload', methods=['POST'])
+@app.route('/api/upload', methods=['POST', 'OPTIONS'])
 def upload_file():
+
+    if request.method == 'OPTIONS':
+        return jsonify({"message": "Options request received"}), 200
+
+    data = request.get_json()
+    id = data['id']
+    url = data['fileUrl']
+
+
+    if not id or not url:
+        return jsonify({"error": "Missing required fields"}), 400
     
     # store file in embeddings (qdrant)
-
-    return jsonify({"message": f"File uploaded successfully"}), 200
-
-@app.route("/chat", methods=["POST"])
-def chat_with_pdf():
     try:
-        retriever = load_vectorstore("sss", "sss")
-        llm = ChatOpenAI(model="gpt-4o")
-        # system_prompt = question_prompt()
-        qa_chain = RetrievalQA.from_chain_type(
-            llm=ChatOpenAI("gpt-4o"),  # Use any supported LLM
-            retriever=retriever,
-        )
+        vectorstore = vectorstore_ingest(url, id)
+    
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+    return jsonify({
+            "message": "File processed successfully",
+        }), 200
+
+# @app.route("/api/chat", methods=["POST"])
+# def chat_with_pdf():
+#     try:
+#         retriever = load_vectorstore("sss", "sss")
+#         llm = ChatOpenAI(model="gpt-4o")
+#         # system_prompt = question_prompt()
+#         qa_chain = RetrievalQA.from_chain_type(
+#             llm=ChatOpenAI("gpt-4o"),  # Use any supported LLM
+#             retriever=retriever,
+#         )
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
--- backend/helpers/preprocessing.py
+++ backend/helpers/preprocessing.py
@@ -2,10 +2,12 @@ from docling.document_converter import DocumentConverter
 
 from langchain.docstore.document import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings 
-from langchain.vectorstores import Qdrant
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
+
+# from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import Qdrant
+
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+# from langchain_qdrant import QdrantVectorStore
 
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance
@@ -13,12 +15,13 @@ from qdrant_client.models import VectorParams, Distance
 import os 
 from dotenv import load_dotenv
 
-load_dotenv("../.env")
+load_dotenv()
 
 qdrant_client = QdrantClient(
-    host=os.getenv("QDRANT_URL"),
+    url=os.getenv("QDRANT_URL"),
     api_key=os.getenv("QDRANT_API_KEY"),
 )
+
 embeddings = OpenAIEmbeddings()
 
 def pdf_processing(url):
@@ -26,42 +29,58 @@ def pdf_processing(url):
     result = converter.convert(url)
     docs = result.document.export_to_markdown()
 
-    
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=512,
-        chunk_overlap=50,
+        chunk_size=300,
+        chunk_overlap=30,
     )
 
-    chunks = text_splitter.split_documents(docs)
+    chunks = text_splitter.split_text(docs)
     documents = []
     for chunk in chunks:
-        content = chunk.page_content
+        content = chunk
         metadata = {
             "source": url
         }
         document = Document(page_content=content, metadata=metadata)
         documents.append(document)
-
     return documents
 
 def vectorstore_ingest(url, id):
     docs = pdf_processing(url)
+    collection_name = id
+
     qdrant_client.recreate_collection(
-    collection_name=id,
-    vectors_config=VectorParams(size=1536, distance=Distance.COSINE))
+        collection_name=collection_name,
+        vectors_config=VectorParams(size=1536, distance=Distance.COSINE)
+    )
+
+    print("Collection created, ingesting documents...")
 
     vectorstore = Qdrant.from_documents(
         documents=docs,
-        embeddings=embeddings,
-        client=qdrant_client,
-        collection_name=id
+        embedding=embeddings,
+        collection_name=collection_name,
+        url=os.getenv("QDRANT_URL"),
+        api_key=os.getenv("QDRANT_API_KEY"),
     )
 
     return vectorstore
 
-def load_vectorstore(url, id):
-    return Qdrant(
-        client=qdrant_client,
-        collection_name=id,
-        embedding=embeddings,
-    ).as_retriever()
+# def load_vectorstore(collection_name):
+#     return Qdrant.from_existing_collection(
+#         embedding=embeddings,
+#         client=qdrant_client,
+#         collection_name=collection_name,
+#         url=os.getenv("QDRANT_URL"),
+#         api_key=os.getenv("QDRANT_API_KEY"),
+#     )
+
+# def setup_qa_chain(collection_name):
+#     retriever = load_vectorstore(collection_name)
+#     llm = ChatOpenAI(model="gpt-4o") 
+#     qa_chain = RetrievalQA.from_chain_type(
+#         llm=llm,
+#         retriever=retriever,
+#         return_source_documents=True
+#     )
+#     return qa_chain
