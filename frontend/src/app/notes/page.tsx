fea9cb2679ef4d7ee6d77c64e3b1655c04ce95b2
jovnc <edtokens@gmail.com>
2025-01-10 20:53:48 +0800

    fix type error

--- frontend/src/app/notes/page.tsx
+++ frontend/src/app/notes/page.tsx
@@ -1,13 +1,19 @@
 import NotesFilter from "@/components/notes/NotesFilter";
 import { getAllNotes } from "@/actions/notes";
+import { Level, Subject } from "@/types/notes";
 
 export default async function NotesPage() {
   const notes = await getAllNotes();
+  const updatedNotes = notes.map(note => ({
+    ...note,
+    level: note.level as Level,
+    subject: note.subject as Subject
+  }));
 
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900 p-8 flex flex-col gap-y-6">
       <h1 className="text-3xl font-bold mb-6 text-black dark:text-white">Uploaded Notes</h1>
-      <NotesFilter notes={notes}/>
+      <NotesFilter notes={updatedNotes}/>
     </div>
   );
 }
