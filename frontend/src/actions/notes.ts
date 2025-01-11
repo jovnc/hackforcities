200a559d429df56c0bd35bd2fc4de35199155cf1
jovnc <edtokens@gmail.com>
2025-01-11 12:17:28 +0800

    final commit

--- frontend/src/actions/notes.ts
+++ frontend/src/actions/notes.ts
@@ -1,55 +1,55 @@
-"use server"
+'use server';
 
-import db from "@/lib/prisma";
-import { Level, Subject } from "@/types/notes";
+import db from '@/lib/prisma';
+import { Level, Subject } from '@/types/notes';
 
 export async function getAllNotes() {
-    try {
-        const res = await db.notes.findMany();
-        return res;
-    } catch (error) {
-        return [];
-    }
+  try {
+    const res = await db.notes.findMany();
+    return res;
+  } catch (error) {
+    return [];
+  }
 }
 
 export async function getNoteById(id: string) {
-    try {
-        const res = await db.notes.findUnique({
-            where: {
-                id: id
-            }
-        });
-        return res;
-    } catch (error) {
-        return null;
-    }
+  try {
+    const res = await db.notes.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return res;
+  } catch (error) {
+    return null;
+  }
 }
 export async function getRecentNotes() {
-    try {
-      const res = await db.notes.findMany({
-        orderBy: {
-          createdAt: "desc", // Adjust this to the appropriate timestamp field in your database
-        },
-        take: 5, // Limit to 5 results
-      });
-    
-      return res.map((note) => ({
-        ...note,
-        level: note.level as Level, // Explicit cast to Level
-        subject: note.subject as Subject,
-      }));
-    } catch (error) {
-      console.error("Error fetching recent notes", error);
-      return [];
-    }
-  }
+  try {
+    const res = await db.notes.findMany({
+      orderBy: {
+        createdAt: 'desc', // Adjust this to the appropriate timestamp field in your database
+      },
+      take: 3, // Limit to 3 results
+    });
 
-  export async function getNotesCount() {
-    try {
-        const count = await db.notes.count();
-        return count;
-    } catch (error) {
-        console.error("Error fetching notes count", error);
-        return 0; // Return 0 if an error occurs
-    }
+    return res.map((note) => ({
+      ...note,
+      level: note.level as Level, // Explicit cast to Level
+      subject: note.subject as Subject,
+    }));
+  } catch (error) {
+    console.error('Error fetching recent notes', error);
+    return [];
+  }
+}
+
+export async function getNotesCount() {
+  try {
+    const count = await db.notes.count();
+    return count;
+  } catch (error) {
+    console.error('Error fetching notes count', error);
+    return 0; // Return 0 if an error occurs
+  }
 }
