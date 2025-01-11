a7e33157f3fd887a8f3cb168333aab9613b2df53
jovnc <edtokens@gmail.com>
2025-01-11 12:18:30 +0800

    final commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,30 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col bg-primary/5">
-      <div className="grid md:grid-cols-2 items-center justify-between p-8">
-        <div className="md:text-start text-center">
-          <h1 className="text-4xl font-bold mb-4">
+    <div className="flex h-full flex-col bg-primary/5">
+      <div className="grid items-center justify-between p-8 md:grid-cols-2">
+        <div className="text-center md:text-start">
+          <h1 className="mb-4 text-4xl font-bold">
             Welcome to <p>SLSmart</p>
           </h1>
           <p className="text-md mb-4 text-muted-foreground">
-            Your ultimate solution for generating customized worksheets. Create,
-            customize, and download worksheets tailored to your needs.
+            Your ultimate solution for generating customized worksheets. Create, customize, and
+            download worksheets tailored to your needs.
           </p>
-          <Button>
-            <Link href="/auth/login">Begin your journey</Link>
+          <Button asChild>
+            <Link href="/dashboard">Begin your journey</Link>
           </Button>
         </div>
-        <div className="px-32 py-16 xs:p-16 md:p-16 xl:p-32 w-full">
+        <div className="xs:p-16 w-full px-32 py-16 md:p-16 xl:p-32">
           <Image
             src="/logo.png"
             alt="WorksheetGen Logo"
             width={400}
             height={400}
-            className="flex justify-center items-center w-full"
+            className="flex w-full items-center justify-center"
           />
         </div>
       </div>
